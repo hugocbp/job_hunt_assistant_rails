02cec71250974581552ed6fd592c739f47fee201
Hugo Carlos Borges Pinto <hugocbp@gmail.com>
2019-02-16 14:04:40 -0800

    Creates app navbar and index page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,5 +7,3 @@ require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
-require("bootstrap");
-require("../../assets/javascript/application.js");
