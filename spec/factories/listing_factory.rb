cdc0ec1d19fe3af47a9632a7258e10bee27392cd
Hugo Carlos Borges Pinto <hugocbp@gmail.com>
2019-02-17 10:36:40 -0800

    Adds and cleans up configuration files

---
+++ spec/factories/listing_factory.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :listing do
+    sequence(:title) { |n| "Test Listing #{n}" }
+    sequence(:company) { |n| "Company #{n}" }
+    sequence(:description) { |n| "Some description #{n}" }
+    technologies { "Ruby on Rails, React" }
+  end
+end
+
+
