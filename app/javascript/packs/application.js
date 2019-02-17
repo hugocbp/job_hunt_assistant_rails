45f49bbd2b9e3658de44241e331a64a1b241edeb
Hugo Carlos Borges Pinto <hugocbp@gmail.com>
2019-02-16 16:37:34 -0800

    Fix FontAwesome for Heroku

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,4 +8,6 @@ require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
 require("bootstrap");
-require("@fortawesome/fontawesome-free/js/all");
+require("@fortawesome/fontawesome-free-webfonts");
+require("@fortawesome/fontawesome-free-webfonts/css/fa-solid.css");
+require("@fortawesome/fontawesome-free-webfonts/css/fa-regular.css");
