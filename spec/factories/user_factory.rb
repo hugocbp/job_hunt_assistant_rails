7072913289cbb83c9458abb7a08a4a9acf6a9801
Hugo Carlos Borges Pinto <hugocbp@gmail.com>
2019-02-16 21:25:20 -0800

    Adds FactoryBot and create user factory

---
+++ spec/factories/user_factory.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :user do
+    email { "test@test.com" }
+    password  { "123123123" }
+  end
+end
--- spec/features/user/sign_in_spec.rb
+++ spec/features/user/sign_in_spec.rb
@@ -1,10 +1,8 @@
 require 'rails_helper'
 
 describe "Signing in" do
-	before do
-		User.create({ email: "test@test.com", password: "123123123" })
-	end
-	
+	let(:user) { FactoryBot.create(:user) }
+
 	context "with correct credentials" do
 		it "authenticates the user" do
 			visit root_url
@@ -12,8 +10,8 @@ describe "Signing in" do
 			click_link "Log In"
 
 			within("#new_user") do
-				fill_in "Email", with: "test@test.com"
-				fill_in "Password", with: "123123123"
+				fill_in "Email", with: user.email
+				fill_in "Password", with: user.password
 			end
 
 			click_button "Log in"
