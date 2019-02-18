4b795151a6415d6259c82e2b81abd383a6f25ecb
Hugo Carlos Borges Pinto <hugocbp@gmail.com>
2019-02-18 11:52:44 -0800

    Adds Technology#new and #create

--- app/controllers/technologies_controller.rb
+++ app/controllers/technologies_controller.rb
@@ -4,4 +4,24 @@ class TechnologiesController < ApplicationController
   def index
     @technologies = current_user.technologies
   end
+
+  def new
+    @technology = current_user.technologies.new
+  end
+
+  def create
+    @technology = current_user.technologies.new(tech_params)
+
+    if @technology.save
+      redirect_to technologies_path, notice: 'Technology created'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def tech_params
+    params.require(:technology).permit(:name)
+  end
 end
---
+++ spec/features/technologies/create_technology_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe 'Creating a new technology' do
+  let(:user) { FactoryBot.create(:user) }
+  before(:each) { sign_in user }
+
+  context 'with valid data' do
+    it 'saves the new technolgy and display all technologies' do
+      visit technologies_url
+
+      click_link 'New Technology'
+      expect(current_path).to eq new_technology_path
+
+      fill_in "technology_name", with: "React"
+      click_button "Create Technology"
+
+      expect(current_path).to eq technologies_path
+      expect(page).to have_content "React"
+    end
+  end
+
+  context 'with invalid data' do
+    it 'does not create the technology and displays errors' do
+      visit new_technology_path
+
+      expect { click_button 'Create Technology' }.not_to change(Technology, :count)
+      expect(page).to have_content("Error!")
+    end
+  end
+end
