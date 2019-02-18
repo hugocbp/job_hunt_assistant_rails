8f6cb182a426f1d7d4c141bb86057b0b33670f72
Hugo Carlos Borges Pinto <hugocbp@gmail.com>
2019-02-18 10:42:36 -0800

    Refactor layout into partials

--- spec/features/pages/home_page_spec.rb
+++ spec/features/pages/home_page_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-describe 'Visiting the home page' do
-	it 'loads the landing page' do
+describe 'The root path' do
+	it 'renders the landing page' do
 		visit root_url
 
 		expect(page).to have_content "Your personal assistant"
