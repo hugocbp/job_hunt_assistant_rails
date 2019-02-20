55bb61af81f71a1a4670533c180bb6b4d392c409
Hugo Carlos Borges Pinto <hugocbp@gmail.com>
2019-02-20 09:02:06 -0800

    Improves listings#show design

--- app/javascript/src/fontawesome.js
+++ app/javascript/src/fontawesome.js
@@ -4,10 +4,12 @@ import { faHeart } from "@fortawesome/free-regular-svg-icons/faHeart";
 import { faPlusSquare } from "@fortawesome/free-regular-svg-icons/faPlusSquare";
 import { faTasks } from "@fortawesome/free-solid-svg-icons/faTasks";
 import { faPlusCircle } from "@fortawesome/free-solid-svg-icons/faPlusCircle";
+import { faChartLine } from "@fortawesome/free-solid-svg-icons/faChartLine";
 
 library.add(faHeart);
 library.add(faPlusSquare);
 library.add(faTasks);
 library.add(faPlusCircle);
+library.add(faChartLine);
 
 dom.watch();
