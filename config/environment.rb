26086d4f7f3c8e6521b7a31041cd358862569b79
Hugo Carlos Borges Pinto <hugocbp@gmail.com>
2019-02-22 14:10:06 -0800

    Configured SendGrid for production

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,15 @@
 # Load the Rails application.
-require_relative 'application'
+require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => Rails.application.credentials.sendgrid[:user_name],
+  :password => Rails.application.credentials.sendgrid[:password],
+  :domain => "yourdomain.com",
+  :address => "smtp.sendgrid.net",
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true,
+}
