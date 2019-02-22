9f9c110c8ba98d4af432ebc45867d88f43b39c06
Hugo Carlos Borges Pinto <hugocbp@gmail.com>
2019-02-22 14:18:25 -0800

    Adds default_url_options for Devise emails

--- config/environment.rb
+++ config/environment.rb
@@ -4,10 +4,12 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
+Rails.application.routes.default_url_options[:host] = "hugocbp-job-hunt-assistant.herokuapp.com"
+
 ActionMailer::Base.smtp_settings = {
   :user_name => Rails.application.credentials.sendgrid[:user_name],
   :password => Rails.application.credentials.sendgrid[:password],
-  :domain => "yourdomain.com",
+  :domain => "hugocbp-job-hunt-assistant.herokuapp.com",
   :address => "smtp.sendgrid.net",
   :port => 587,
   :authentication => :plain,
